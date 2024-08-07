50263ef8e337215a82004d0e7d416928eee7ed97
Abdur-Rehman Rizvi <156486312+rizvi5299@users.noreply.github.com>
2024-08-07 13:41:06 -0400

    Create main.java
    Main class to start-up the game.

---
+++ main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HANGMAN start = new HANGMAN();
+        Scanner scanner = new Scanner(System.in);
+        boolean playagain = true;
+
+
+        while (playagain) {
+            System.out.println("Welcome to hangman! Guess the word by guessing letters at a time before your number of incorrect guesses runs left!");
+            System.out.print("Choose your difficulty, 'Easy', 'Medium', or 'Hard': ");
+            while (true) {
+                String difficultyinput = scanner.next();
+                if (difficultyinput.equalsIgnoreCase("easy")) {
+                    start.hangmangame(HANGMAN.easyWords, 13, "easy");
+                    break;
+                } else if (difficultyinput.equalsIgnoreCase("medium")) {
+                    start.hangmangame(HANGMAN.mediumWords,15, "medium");
+                    break;
+                } else if (difficultyinput.equalsIgnoreCase("hard")) {
+                    start.hangmangame(HANGMAN.hardWords,14, "hard");
+                    break;
+                } else System.out.print("Please only enter the difficulty: Easy, Medium, or Hard. ");
+            }
+
+            System.out.print("That was fun! Do you want to play again? (yes/no): ");
+            String playAgainResponse = scanner.next();
+            playagain = playAgainResponse.equalsIgnoreCase("yes");
+        }
+        System.out.print("\nGOODBYE! See you next time :)\n");
+    }
+}
